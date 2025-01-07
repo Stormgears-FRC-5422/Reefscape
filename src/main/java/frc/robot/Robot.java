9a09d9059292fbcd7cf96a02f0798d3fdeeaf3f6
darrenleomiller <darrenleomiller@gmail.com>
2025-01-07 11:31:43 -0500

    Added basic AdvantageKit 4.0.0

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,22 +4,62 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
-  private final RobotContainer m_robotContainer;
+public class Robot extends LoggedRobot {
+  private Command autonomousCommand;
+
+  private final RobotContainer robotContainer;
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
+      Logger.recordMetadata("ProjectName", Constants.robotName); // Set a metadata value
+      Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+      Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+      Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+      Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+      switch (BuildConstants.DIRTY) {
+          case 0:
+              Logger.recordMetadata("GitDirty", "All changes committed");
+              break;
+          case 1:
+              Logger.recordMetadata("GitDirty", "Uncomitted changes");
+              break;
+          default:
+              Logger.recordMetadata("GitDirty", "Unknown");
+              break;
+      }
+
+      if (isReal()) {
+          // TOOD - figure out what happens if there is no USB drive plugged in?
+          Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+          Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+          //new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
+      } else {
+          setUseTiming(false); // Run as fast as possible
+          String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+          Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+          Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+      }
+
+      Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
+      robotContainer = new RobotContainer();
   }
 
   @Override
   public void robotPeriodic() {
+    Threads.setCurrentThreadPriority(true, 99);
     CommandScheduler.getInstance().run();
+    Threads.setCurrentThreadPriority(false, 10);
   }
 
   @Override
@@ -33,10 +73,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    autonomousCommand = robotContainer.getAutonomousCommand();
 
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
     }
   }
 
@@ -48,8 +88,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
     }
   }
 
