653c130f885650502005a58ffca3b1706a059d06
Krish <krish.2008parmar@gmail.com>
2025-02-10 22:26:19 -0500

    auto

--- src/main/java/frc/robot/commands/autos/AutoCommandFactory.java
+++ src/main/java/frc/robot/commands/autos/AutoCommandFactory.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.drive.DrivetrainBase;
+import org.littletonrobotics.junction.Logger;
 
 import java.util.Optional;
 
@@ -21,16 +22,18 @@ public class AutoCommandFactory extends Command {
     private static final PIDController xController = new PIDController(0, 0.0, 0.0);
     private static final PIDController yController = new PIDController(0, 0.0, 0.0);
     private static final PIDController headingController = new PIDController(0, 0.0, 0.0);
-    private final Timer timer = new Timer();
+    private Timer timer;
 
 
     public AutoCommandFactory(DrivetrainBase drivetrainBase) {
         this.drivetrainBase = drivetrainBase;
+        timer = new Timer();
     }
 
-    public static void followTrajectory(SwerveSample sample) {
+    public  void followTrajectory(SwerveSample sample) {
         // Get the current pose of the robot
         Pose2d pose = drivetrainBase.getPose();
+        Logger.recordOutput("pose trj", sample.getPose());
 
         // Generate the next speeds for the robot
         ChassisSpeeds speeds = new ChassisSpeeds(
@@ -38,16 +41,32 @@ public class AutoCommandFactory extends Command {
             sample.vy + yController.calculate(pose.getY(), sample.y),
             sample.omega + headingController.calculate(pose.getRotation().getRadians(), sample.heading)
         );
-
+        Logger.recordOutput("header", sample.heading);
+        Logger.recordOutput("omega", sample.omega);
+        System.out.println(speeds);
         drivetrainBase.drive(speeds, true);
     }
 
     @Override
+    public void initialize() {
+        timer.restart();
+    }
+
+    @Override
     public void execute() {
-        Commands.runOnce(timer::restart);
-        Optional<SwerveSample> sample = trajectory.get().sampleAt(timer.get(), false);
+//        Optional<SwerveSample> sample = trajectory.get().sampleAt(timer.get(), false);
+        Optional<SwerveSample> sample = trajectory.get().sampleAt(0.27225, false);
+        System.out.println("t " + timer.get());
 
-        sample.ifPresent(AutoCommandFactory::followTrajectory);
+        if (sample.isPresent()) {
+            followTrajectory(sample.get());
+        } else{
+            System.out.println("no");
+        }
 
+    }
+
+    @Override
+    public void end(boolean interrupted) {
     }
 }
